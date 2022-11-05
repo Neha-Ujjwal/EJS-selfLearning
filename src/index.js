e0042a6e76f2d8e5f199b1e1ba5c4a7d09adf6b1
Neha-Ujjwal <nehaujjwal1614@gmail.com>
2022-11-05 10:39:48 +0530

    partials added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
  const express=require('express');
  const app=express();
  const path=require('path');
+ const hbs=require('hbs');
  const port=8000;
 
  //path to static files
 const staticPath=path.join(__dirname,'../public');
 
+//pah of template folder
+const templatePath=path.join(__dirname+'/template/views');
+const partialPath=path.join(__dirname+'/template/partials');
+
+
 //to set the view engine
 app.set('views')
 app.set('view engine','hbs');
+
+//we can the name of this view folder by.suppose the new name is template.
+app.set('views',templatePath);
+
+hbs.registerPartials(partialPath); //to use partials
+
 
 
 //to use static files.It will automatically render index.html file
